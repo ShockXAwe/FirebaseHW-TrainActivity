91e7a6a221e371423dc03e6a0fd547ac80cb024f
Eddie <eddie.a.linares@gmail.com>
2019-04-28 19:21:33 -0500

    Tried to fix logic, broken..

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -19,33 +19,32 @@ $("#add-train").on("click", function (event) {
     var destination = $("#destination-input").val().trim();
     var firstTrain = $("#first-train-input").val().trim();
     var frequency = $("#frequency-input").val().trim();
-
+    // Convert the first train to military time
     var momentTrain = moment(firstTrain, "HH:mm A").format("HH:mm")
     console.log(momentTrain);
-
-    // var momentInst = moment(start, "MM/DD/YYY")
-    // var startMoment = moment(start).toNow();
-    // console.log(startMoment);
-    
-    // var tBody = $("tbody");
-    // var tRow = $("<tr>");
-
-    // var nameTd = $("<td>").text(train);
-    // var roleTd = $("<td>").text(destination);
-    // var startTd = $("<td>").text(firstTrain);
-    // var monthlyWorkedTd = $("<td>").text("test");
-    // var monthlyRateTd = $("<td>").text(rate);
-    // var totalTd = $("<td>").text("Too much son!!");
-
-
-    // tRow.append(nameTd, roleTd, startTd, monthlyWorkedTd, monthlyRateTd, totalTd);
-    // tBody.append(tRow);
+    // Grab the current time
+    var currentTime = moment().format("HH:mm");
+    console.log(currentTime);
+    // Grab the difference of the current time with the upcoming train
+    var differenceTime = momentTrain.diff(currentTime, "minutes") * -1;
+    console.log("Difference between now entered time", differenceTime);
+    // Get the remainder using %. Ex 10 divided by 3 = 3 with remaidner of 1. % finds
+    var remainderCalc = differenceTime % frequency;
+    console.log("Math Difference", remainderCalc);
+    // Subtract the remainder from the frequency and store in a variable
+    var untilArrival = frequency - remainderCalc
+    console.log("until arrival is:", untilArrival);
+    // Get the future time
+    var futureTrainTime = currentTime.add(untilArrival, "m").format("HH:mm")
+    console.log("future train time:" + futureTrainTime);
 
     var trainInfo = {
         train: train,
         destination: destination,
         momentTrain: momentTrain,
-        frequency: frequency
+        frequency: frequency,
+        futureTrainTime: futureTrainTime,
+        untilArrival:untilArrival
     }
 
     database.ref().push(trainInfo);
@@ -64,10 +63,8 @@ database.ref().on("child_added", function (child) {
     var trainDestination = child.val().destination;
     var trainFrequency = child.val().frequency;
     var trainFirst = child.val().momentTrain;
-
-
-    // var momentInst = moment(empStart, "MM/DD/YYY")
-    // var empMonths = momentInst.diff(moment(), "months") * -1;
+    var trainNext = child.val().futureTrainTime;
+    var trainETA = child.val().untilArrival;
 
 
     var tBody = $("tbody");
@@ -76,8 +73,8 @@ database.ref().on("child_added", function (child) {
     var trainTd = $("<td>").text(trainName);
     var destinationTd = $("<td>").text(trainDestination);
     var trainFrequencyTd = $("<td>").text(trainFrequency);
-    var nextArrivalTd = $("<td>").text("Logic needed for next arrival!!");
-    var minutesAwayTd = $("<td>").text("Logic needed for minutes away!!");
+    var nextArrivalTd = $("<td>").text(trainNext);
+    var minutesAwayTd = $("<td>").text(trainETA);
     var firstTrainTd = $("<td>").text(trainFirst);
 
     tRow.append(trainTd, destinationTd, trainFrequencyTd, nextArrivalTd, minutesAwayTd);
